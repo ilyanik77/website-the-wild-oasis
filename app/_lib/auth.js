6ba9acba90b1de24c5926432a86ecef877bfccde
ilya <ilyakhlamov77@gmail.com>
2025-02-20 08:04:33 +0300

    add 19.02.2025

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth'
 import Google from 'next-auth/providers/google'
+import { createGuest, getGuest } from './data-service'
 
 const authConfig = {
 	providers: [
@@ -8,9 +9,33 @@ const authConfig = {
 			clientSecret: process.env.AUTH_GOOGLE_SECRET,
 		}),
 	],
+	callbacks: {
+		authorized({auth, request}) {
+			return !!auth?.user
+		},
+		async signIn({user, account, profile}) {
+			try { 
+				const existingGuest = await getGuest(user.mail)
+				if(!existingGuest) await createGuest({email: user.email, fullName: user.name})
+				return true
+			} catch (error) {
+				return false
+			}
+		},
+		async session({session, user}) {
+			const quest = await getGuest(session.user.email)
+			session.user.questId = quest.questId
+			return session 
+		}
+	},
+	pages: {
+		signIn: '/login'
+	}
 }
 
 export const {
 	auth,
+	signIn,
+	signOut,
 	handlers: { GET, POST },
 } = NextAuth(authConfig)
--- app/account/page.js
+++ app/account/page.js
@@ -1,12 +1,18 @@
+import { auth } from "../_lib/auth"
+
 export const metadata = {
 	title: 'Guest area',
 }
 
 
-export default function Page() {
+export default async function Page() {
+
+	const session = await auth()
+	const firstName = session.user.name.split(' ').at(0)
+
 	return (
 		<h2 className='font-semibold text-2xl text-accent-400 mb-4'>
-			Welcome, User
+			Welcome, {firstName}
 		</h2>
 	)
 }
